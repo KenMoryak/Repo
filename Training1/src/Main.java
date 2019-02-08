4b1e694f7fe18581ec330100ebe76e96e42b14d7
KenMoryak <k_konopihin@mail.ru>
2019-02-09 00:35:16 +0300

    initial

---
+++ Training1/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    public static void main(String[] args) {
+        double a = 65.73;
+        double b = 74.43;
+        String c = USD, d = EUR;
+        double summusd = a * e;
+        double summeur = b * e;
+        string f;
+        int e;
+
+        System.out.println("This is currency exchange");
+        System.out.println("Input сurrency");
+        Scanner scanner = new Scanner(System.in);
+        sc.read(f);
+            if (e = USD) {
+                System.out.println("Input amount");
+                Scanner scanner = new Scanner(System.in);
+                sc.read(e);
+                System.out.println(summusd);
+            }
+
+
+            else {
+                System.out.println("Input amount");
+                Scanner scanner = new Scanner(System.in);
+                sc.read(e);
+                System.out.println(summeur);
+            }
+
+    }
+}
